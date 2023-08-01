3601c6f6bb039b13d92cf708426a2ea66edaec63
何喜阳 <iamxiyang@foxmail.com>
2023-08-01 20:41:31 +0800

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import { scan } from 'qr-scanner-wechat'
+
+function App() {
+  return <div className="text-center">Hi</div>
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import React from '@vitejs/plugin-react'
+import UnoCSS from 'unocss/vite'
+import presetAttributify from '@unocss/preset-attributify'
+import presetIcons from '@unocss/preset-icons'
+import presetUno from '@unocss/preset-uno'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    UnoCSS({
+      shortcuts: [
+        { logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180' },
+      ],
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          extraProperties: {
+            'display': 'inline-block',
+            'vertical-align': 'middle',
+          },
+        }),
+      ],
+    }),
+    React(),
+  ],
+})
