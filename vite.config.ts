56f3e5e51b3e6fb1e3d70fcd5be5f77f7e789461
何喜阳 <iamxiyang@foxmail.com>
2023-08-05 22:19:50 +0800

    chore: 删减依赖，调整编译配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,19 @@
 import { defineConfig } from 'vite'
 import React from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
-import presetAttributify from '@unocss/preset-attributify'
-import presetIcons from '@unocss/preset-icons'
-import presetUno from '@unocss/preset-uno'
+import presetWind from '@unocss/preset-wind'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    minify: true,
+    target: 'es2019',
+  },
   plugins: [
     UnoCSS({
-      shortcuts: [
-        { logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180' },
-      ],
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons({
-          extraProperties: {
-            'display': 'inline-block',
-            'vertical-align': 'middle',
-          },
-        }),
-      ],
+      shortcuts: [{ logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180' }],
+      presets: [presetWind()],
     }),
     React(),
   ],
