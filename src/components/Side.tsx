e8973d053e6d86f7cc29ae47d2a598e0581a2e9f
何喜阳 <iamxiyang@foxmail.com>
2023-12-24 20:31:46 +0800

    feat: 解码历史支持搜索

--- src/components/Side.tsx
+++ src/components/Side.tsx
@@ -1,11 +1,24 @@
 import { ClearOutlined } from '@ant-design/icons'
-import { Popconfirm, Button } from 'antd'
+import { Popconfirm, Button, Input } from 'antd'
 import { HistoryItem } from './HistoryItem'
 import { useProxy } from 'valtio/utils'
 import { state } from '../store'
+import { useMemo, useState } from 'react'
 
 export const Side = () => {
+  const [text, setText] = useState('')
   const history = useProxy(state).decodeHistory
+
+  const filteredHistory = useMemo(() => {
+    try {
+      if (!text) return history
+      const reg = new RegExp(text)
+      return history.filter(item => reg.test(item))
+    } catch (e) {
+      return history
+    }
+  }, [text, history])
+
   return (
     <section className="w-30% h-full bg-#f2f2f2 dark:bg-#141414 overflow-hidden px-10px ml-10px rd-6px">
       <header className="flex items-center justify-between h-52px py-10px">
@@ -23,8 +36,16 @@ export const Side = () => {
           </Popconfirm>
         )}
       </header>
+      <Input
+        placeholder="搜索记录/支持正则"
+        className="mb-12px mr-6px"
+        value={text}
+        onChange={e => {
+          setText(e.target.value)
+        }}
+      />
       <div className="overflow-y-auto overflow-x-hidden h-[calc(100%-52px)] p-r-6px">
-        {history.map((item, index) => (
+        {filteredHistory.map((item, index) => (
           <HistoryItem key={index} text={item} />
         ))}
       </div>
